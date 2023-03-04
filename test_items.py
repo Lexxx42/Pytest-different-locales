c74a8fe053da2d5e161704970630d5290c4ab599
Alexander Konukhov <jesusmisha@gmail.com>
2023-03-04 21:42:23 +0300

    Update test_items.py
    changed delay time

--- test_items.py
+++ test_items.py
@@ -9,6 +9,5 @@ URL = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 def test_guest_should_be_able_to_add_product_to_basket(browser):
     ''' Test checking that button Add to basket is on the page. '''
     browser.get(URL)
-    time.sleep(30)
-    add_to_basket_button = browser.find_elements(By.CSS_SELECTOR, '.btn-add-to-basket')
-    assert len(add_to_basket_button) > 0, 'button not found'
+    time.sleep(5)
+    assert browser.find_element(By.CSS_SELECTOR, '.btn-add-to-basket'), 'button not found'
