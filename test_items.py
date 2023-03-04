17a757547aed5df9e1369c7aef739555903ce85e
Alexander Konukhov <jesusmisha@gmail.com>
2023-03-04 20:54:07 +0300

    added assert

--- test_items.py
+++ test_items.py
@@ -10,4 +10,5 @@ def test_guest_should_be_able_to_add_product_to_basket(browser):
     ''' Test checking that button Add to basket is on the page. '''
     browser.get(link)
     time.sleep(30)
-    browser.find_element(By.CSS_SELECTOR, '.btn-add-to-basket')
+    add_to_basket_button = browser.find_elements(By.CSS_SELECTOR, '.btn-add-to-basket')
+    assert len(add_to_basket_button) > 0, 'button not found'
