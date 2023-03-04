8317cf69ed90b25f0d8dea18b6081d41f3784e3e
Alexander Konukhov <jesusmisha@gmail.com>
2023-03-04 20:39:22 +0300

    added test for checking the Add to basket button

--- conftest.py
+++ conftest.py
@@ -1,3 +1,5 @@
+''' Configuration file for Pytest module. '''
+
 import pytest
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options as firefoxOptions
@@ -5,30 +7,31 @@ from selenium.webdriver.chrome.options import Options as chromeOptions
 
 LANGUAGES = ['ru', 'en', 'fr', 'es']
 
+
 def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default=None,
                      help="Choose browser: chrome or firefox")
     parser.addoption('--language', action='store', default='en',
-                     help=f"Choose language: {LANGUAGES}")
+                     help=f'Choose language: {LANGUAGES}')
 
 
-@pytest.fixture(scope="function")
+@pytest.fixture(scope='function')
 def browser(request):
-    browser_name = request.config.getoption("browser_name")
-    language = request.config.getoption("language")
+    browser_name = request.config.getoption('browser_name')
+    language = request.config.getoption('language')
     browser = None
-    if browser_name == "chrome":
+    if browser_name == 'chrome':
         options = chromeOptions()
         options.add_experimental_option('prefs', {'intl.accept_languages': language})
-        print("\nstart chrome browser for test..")
+        print('\nstart chrome browser for test..')
         browser = webdriver.Chrome(options=options)
-    elif browser_name == "firefox":
+    elif browser_name == 'firefox':
         options = firefoxOptions()
-        options.set_preference("intl.accept_languages", language)
-        print("\nstart firefox browser for test..")
+        options.set_preference('intl.accept_languages', language)
+        print('\nstart firefox browser for test..')
         browser = webdriver.Firefox(options=options)
     else:
-        raise pytest.UsageError("--browser_name should be chrome or firefox")
+        raise pytest.UsageError('--browser_name should be chrome or firefox')
     yield browser
-    print("\nquit browser..")
+    print('\nquit browser..')
     browser.quit()
--- test_items.py
+++ test_items.py
@@ -1,8 +1,13 @@
+''' Tests for purchase order. '''
+
+import time
 from selenium.webdriver.common.by import By
 
-link = "http://selenium1py.pythonanywhere.com/"
+link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 
-def test_guest_should_see_login_link(browser):
+def test_guest_should_be_able_to_add_product_to_basket(browser):
+    ''' Test checking that button Add to basket is on the page. '''
     browser.get(link)
-    browser.find_element(By.CSS_SELECTOR, "#login_link")
+    time.sleep(30)
+    browser.find_element(By.CSS_SELECTOR, '.btn-add-to-basket')
